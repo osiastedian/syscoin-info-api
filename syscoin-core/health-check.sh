c9722741dae8e950f6bcca2658a596225108c4d9
Ted Ian Osias <osiastedian@gmail.com>
2022-12-07 08:06:05 +0800

    update health check

--- syscoin-core/health-check.sh
+++ syscoin-core/health-check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-CURRENT=$(curl --data-binary '{"jsonrpc":"1.0","method":"getblockchaininfo", "params": []}' http://syscoin:bVPjjYNqV1lnK8NxoK-hiPHNQLU8bhJgEfQ-DjLVcdU%3D@ec2-34-221-218-122.us-west-2.compute.amazonaws.com:8370 | jq '.result.headers')
+FETCH_URI="http://$RPC_AUTH@localhost:8370"
+CURRENT=$(curl --data-binary '{"jsonrpc":"1.0","method":"getblockchaininfo", "params": []}' $FETCH_URI | jq '.result.headers')
 LATEST=$(curl https://blockbook.elint.services/api/ | jq '.backend.headers')
 
 test $CURRENT -eq $LATEST
